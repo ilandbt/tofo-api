8507b47869c0ef8ad42c852df421cb40b1079d1f
Ilan Ben Tal <ilandbt@gmail.com>
2016-06-17 18:02:28 +0300

    Added: GET todos, GET todos/:id

--- server.js
+++ server.js
@@ -2,8 +2,47 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+//mock DB
+var todos = [{
+	id: 1,
+	desceiption: 'Drink warter',
+	completed: false
+}, {
+	id: 2,
+	desceiption: 'Eat stake',
+	completed: false
+}, {
+	id: 3,
+	desceiption: 'Sleep',
+	completed: true
+}];
+
 app.get('/', function(req, res) {
 	res.send('Todo api root');
+});
+
+//get all todos list
+
+app.get('/todos', function(req, res) {
+	res.json(todos);
+});
+
+//get todo item by id
+app.get('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id. 10);
+	var matchedTodo;
+	
+	todos.forEach(function(todoItem){
+		if (todoId === todoItem.id) {
+			matchedTodo = todoItem;
+		}
+	});
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
 });
 
 app.listen(PORT, function() {
