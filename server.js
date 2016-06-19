718bf0f16eb05479ff7c48e285b4e958e8ae642e
Ilan Ben Tal <ilandbt@gmail.com>
2016-06-19 17:25:17 +0300

    final static todos

--- server.js
+++ server.js
@@ -30,6 +30,14 @@ app.get('/todos', function(req, res) {
 		filteredTodos = _.where(filteredTodos, {completed: false});
 	}
 
+	//added query params - description
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		// filteredTodos = _.where(filteredTodos, {description: queryParams.q });
+		filteredTodos = _.filter(filteredTodos, function(todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
+	} 
+
 	res.json(filteredTodos);
 });
 
