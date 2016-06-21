c3a039160ea159996a5a28fd361866f437d45c99
Ilan Ben Tal <ilandbt@gmail.com>
2016-06-21 15:11:50 +0300

    export app

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 
+var app = require('./app.js');
 
-var app = express();
 var PORT = process.env.PORT || 3000;
 
 //mock DB
