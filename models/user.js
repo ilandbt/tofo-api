85530904f17deb86b724d2599bd5a3eec67afab9
Ilan Ben Tal <ilandbt@gmail.com>
2016-06-21 14:56:16 +0300

    Added User model and POST /Users request

---
+++ models/user.js
@@ -0,0 +1,19 @@
+module.exports = function(sequalize, DataTypes) {
+	return sequalize.define('user', {
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true
+			}
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [7, 100]
+			}
+		}
+	});
+};
--- server.js
+++ server.js
@@ -142,6 +142,24 @@ app.put('/todos/:id', function(req, res) {
 		});
 });
 
+
+
+//USER//
+//post new todo item
+app.post('/users', function(req, res) {
+	//get only completed, description fields
+	var body = _.pick(req.body, "email", "password");
+
+	db.user.create(body)
+		.then(function(user) {
+			res.json(user.toJSON());
+
+		}, function(error) {
+			console.log(error);
+			return res.status(400).json(error);
+		});
+});
+
 db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening to port : ' + PORT);
