1b0d63253cb4fcca4d442c4d8d451802ac660f7b
Ilan Ben Tal <ilandbt@gmail.com>
2016-06-22 08:59:02 +0300

    Added jwt to user login

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function(sequalize, DataTypes) {
 	var user = sequalize.define('user', {
@@ -70,6 +72,27 @@ module.exports = function(sequalize, DataTypes) {
 			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'email', 'id', 'createdAt', 'updatedAt');
+			},
+			generateToken: function(type) {
+				if (!_.isString(type)) {
+					return undefined;
+				}
+
+				try {
+					var stringData = JSON.stringify({
+						id: this.get('id'),
+						type: type
+					});
+					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123#$%').toString();
+					var token = jwt.sign({
+						token: encryptedData
+					}, 'qwr1234');
+
+					return token;
+				} catch (error) {
+					console.log(error);
+					return undefined;
+				}
 			}
 		}
 	});
