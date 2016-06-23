6b700a627fbec3075fcf1d4f366a27ff5084d4fd
Ilan Ben Tal <ilandbt@gmail.com>
2016-06-23 10:26:25 +0300

    Added logout

--- server.js
+++ server.js
@@ -182,19 +182,31 @@ app.post('/users', function(req, res) {
 app.post('/users/login', function(req, res) {
 
 	var body = _.pick(req.body, "email", "password");
+	var userInstance;
 
 	db.user.authenticate(body)
 		.then(function(user) {
 			var token = user.generateToken('authentication');
-			if (token) {
-				res.header('Auth', token).json(user.toPublicJSON());
-			} else {
-				res.status(401).send();
-			}
-		}, function() {
+			userInstance = user;
+			return db.token.create({
+				token: token
+			});
+		}).then(function(token) {
+			res.header('Auth', token.token).json(userInstance.toPublicJSON());
+		}).catch(function() {
 			res.status(401).send();
 		});
 
+});
+
+//login
+app.post('/users/logout', middleware.requireAuthentication, function(req, res) {
+	req.token.destroy()
+		.then(function() {
+			res.status(204).send();
+		}).catch(function() {
+			res.status(500).send();
+		})
 });
 
 db.sequelize.sync({
